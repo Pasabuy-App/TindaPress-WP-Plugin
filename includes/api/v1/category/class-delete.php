21a4b133845d0a6b54b7c464923a974f55ccdd01
Lorz Becislao <lorzbecislao@gmail.com>
2020-08-12 13:06:50 +0800

    Added hotfix in deletion

--- includes/api/v1/category/class-delete.php
+++ includes/api/v1/category/class-delete.php
@@ -54,7 +54,7 @@
             if ( !isset($_POST["catid"])  ) {
 				return array(
 						"status" => "unknown",
-						"message" => "Please contact your administrator. Request Unknown!",
+						"message" => "Please contact your administrator. Request Unknown!!!!!!",
                 );
             }
 
