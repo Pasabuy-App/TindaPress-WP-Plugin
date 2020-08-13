ca2088e4fe39d5b19f26da1921af0e5629ec7a21
Miguel Radaza <48942632+MiguelRadaza@users.noreply.github.com>
2020-08-13 19:18:26 +0800

    Added Rest api for product listing acitve, inactive, stid and status

--- includes/api/v1/products/class-listing.php
+++ includes/api/v1/products/class-listing.php
@@ -22,6 +22,32 @@
             $table_product = TP_PRODUCT_TABLE;
             $table_categories = TP_CATEGORIES_TABLE;
 
+
+            // Step 1: Check if prerequisites plugin are missing
+            $plugin = TP_Globals::verify_prerequisites();
+            if ($plugin !== true) {
+                return array(
+                        "status" => "unknown",
+                        "message" => "Please contact your administrator. ".$plugin." plugin missing!",
+                );
+            }
+			
+			// Step 2: Validate user
+			if (DV_Verification::is_verified() == false) {
+                return array(
+                        "status" => "unknown",
+                        "message" => "Please contact your administrator. verification issues!",
+                );
+                
+            }
+            
+            if (!isset($_POST['stid']) || !isset($_POST['catid']) || !isset($_POST['status'])  ) {
+                return array(
+                    "status" => "unknown",
+                    "message" => "Please contact your admininstrator. Missing paramiters!"
+                );
+            }
+
             $sql = "SELECT
                 tp_prod.ID,
                 tp_prod.stid,
@@ -42,24 +68,61 @@
                 INNER JOIN tp_revisions tp_rev ON tp_rev.ID = tp_prod.title
                 INNER JOIN tp_categories c ON c.ID = tp_prod.ctid  ";
 
-            
             if (isset($_POST['stid']) && $_POST['stid'] > 0  ) {
 
-                $sql .= "WHERE cat.`stid` = '$stid' AND cat.types = 'product' ";
+                $stid = $_POST['stid'];
+
+                $sql .= "WHERE tp_prod.`stid` = '$stid'  ";
+
+                $type_all = $_POST['status'] == '0'? true:false; 
 
             }
-        
-           
 
-        return $wpdb->get_results($sql);
-                    
+            if (isset($_POST['catid']) && $_POST['catid'] > 0  ) {
                 
+                $cat_stats = $_POST['catid'] == '0' ? false:true;
 
+                if($cat_stats == true){
+                    $ctid = $_POST['catid'];
+                    $sql .="AND tp_prod.ctid = $ctid ";
+                }
 
-            
+            }
 
+            if (isset($_POST['status']) && $_POST['status'] > 0 ) {
+                
+                $all = $_POST['status'] == '0'? true:false; 
 
+                $status = $_POST['status'] == '2'? '0':'1';
 
+                if ($all == false  ) {
+                    if (isset($type_all) && $type_all == true) {
+                        $sql .= "WHERE ";
+
+                    }else{
+                        $sql .= "AND ";
+
+                    }
+                    $sql .= " ( SELECT tp_rev.child_val FROM tp_revisions tp_rev WHERE ID = tp_prod.STATUS ) = $status  ";
+                    
+                }
+
+            }
+
+            $results =  $wpdb->get_results($sql);
+            if (!$results) {
+                return array(
+                    "status" => "failed",
+                    "message" => "No resuls found",
+                );
+
+            }else{
+                return array(
+                    "status" => "success",
+                    "data" => $results,
+                );
+
+            }
         }
         
     }
