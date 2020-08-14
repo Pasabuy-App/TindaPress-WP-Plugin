d2217db97596f642a7301c19a5b6ff2539d6a2de
Miguel Radaza <48942632+MiguelRadaza@users.noreply.github.com>
2020-08-14 19:49:01 +0800

    Fixed file name for products nearme and store nearme and added documentation in both modules

---
+++ includes/api/v1/products/class-product-nearme.php
@@ -0,0 +1,107 @@
+<?php
+	// Exit if accessed directly
+	if ( ! defined( 'ABSPATH' ) ) 
+	{
+		exit;
+	}
+
+	/** 
+        * @package tindapress-wp-plugin
+        * @version 0.1.0
+	*/
+    class TP_Product_Nearme {
+
+        public static function listen(){
+            return rest_ensure_response( 
+                TP_Product_Nearme::listen_open()
+            );
+        }
+        
+        public static function listen_open(){
+            global $wpdb;
+
+            $table_revision = TP_REVISIONS_TABLE;
+            $table_product = TP_PRODUCT_TABLE;
+            $table_dv_revision = DV_REVS_TABLE;
+            $table_address = DV_ADDRESS_TABLE;
+            $table_store = TP_STORES_TABLE;
+            $table_category = TP_CATEGORIES_TABLE;
+
+            // Step 1: Check if prerequisites plugin are missing
+            $plugin = TP_Globals::verify_prerequisites();
+            if ($plugin !== true) {
+                return array(
+                        "status" => "unknown",
+                        "message" => "Please contact your administrator. ".$plugin." plugin missing!",
+                );
+            }
+
+            // Step 2: Validate user
+            if (DV_Verification::is_verified() == false) {
+                return array(
+                        "status" => "unknown",
+                        "message" => "Please contact your administrator. Verification issues!",
+                );
+            }
+            // Step 3: Validate listen
+            if (!isset($_POST['lat']) || !isset($_POST['long']) ) {
+                return array(
+                    "status" => "unknown",
+                    "message" => "Please contact your administrator. Request unknown missing paramiters!",
+                );
+            }
+            // Step 3: Validate listen
+            if (empty($_POST['lat']) || empty($_POST['long']) ) {
+                return array(
+                    "status" => "failed",
+                    "message" => "Required fileds cannot be empty.",
+                );
+            }
+
+            // Step 4: Variable declaration 
+            $lat2 = $_POST['lat'];
+            $long2 = $_POST['long'];
+
+            // Step 5: Start query
+            $results = $wpdb->get_results("SELECT
+                tp_prod.ID,
+                tp_prod.stid,
+                tp_prod.ctid AS catid,
+                ( SELECT tp_rev.child_val FROM $table_revision tp_rev WHERE ID = ( SELECT `title` FROM $table_store WHERE ID = tp_prod.stid ) ) AS `store_name`,
+                ( SELECT tp_rev.child_val FROM $table_revision tp_rev WHERE ID = c.title ) AS `cat_name`,
+                ( SELECT tp_rev.child_val FROM $table_revision tp_rev WHERE tp_rev.ID = tp_prod.title ) AS product_name,
+                ( SELECT tp_rev.child_val FROM $table_revision tp_rev WHERE ID = tp_prod.short_info ) AS `short_info`,
+                ( SELECT tp_rev.child_val FROM $table_revision tp_rev WHERE ID = tp_prod.long_info ) AS `long_info`,
+                ( SELECT tp_rev.child_val FROM $table_revision tp_rev WHERE ID = tp_prod.sku ) AS `sku`,
+                ( SELECT tp_rev.child_val FROM $table_revision tp_rev WHERE ID = tp_prod.price ) AS `price`,
+                ( SELECT tp_rev.child_val FROM $table_revision tp_rev WHERE ID = tp_prod.weight ) AS `weight`,
+                ( SELECT tp_rev.child_val FROM $table_revision tp_rev WHERE ID = tp_prod.dimension ) AS `dimension`,
+            IF  ( ( SELECT tp_rev.child_val FROM $table_revision tp_rev WHERE ID = tp_prod.STATUS ) = 1, 'Active', 'Inactive' ) AS `status`,
+                ROUND(  (SELECT `distance`( 
+                        (SELECT child_val FROM $table_dv_revision WHERE ID = 	( SELECT `latitude` FROM $table_address WHERE ID = ( SELECT `address` FROM $table_store WHERE ID = tp_prod.stid ) AND types = 'business' ) AND revs_type ='address' ), 
+                        (SELECT child_val FROM $table_dv_revision WHERE ID = 	( SELECT `longitude` FROM $table_address WHERE ID = ( SELECT `address` FROM $table_store WHERE ID = tp_prod.stid ) AND types = 'business' ) AND revs_type ='address' ), 
+                        '$lat2', 
+                        '$long2'  
+                        )),
+                    3 ) as distance
+            FROM
+                $table_product tp_prod
+                INNER JOIN $table_revision tp_rev ON tp_rev.ID = tp_prod.title
+                INNER JOIN $table_store c ON c.ID = tp_prod.ctid");
+
+            // Step 3: Return results
+            if (!$results) {
+                return array(
+                    "status" => "success",
+                    "message" => "No results found."
+                );
+
+            }else{
+                return array(
+                    "status" => "success",
+                    "data" => $results
+                );
+            }
+            
+        }
+    }
---
+++ includes/api/v1/stores/class-store-nearme.php
@@ -0,0 +1,108 @@
+<?php
+	// Exit if accessed directly
+	if ( ! defined( 'ABSPATH' ) ) 
+	{
+		exit;
+	}
+
+	/** 
+        * @package tindapress-wp-plugin
+        * @version 0.1.0
+	*/
+
+    class TP_Store_Nearme {
+        
+        //REST API Call
+        public static function listen(){
+            return rest_ensure_response( 
+                TP_Store_Nearme:: listen_open()
+            );
+        }
+
+        //QA done 2020-08-12 8:09 pm
+        public static function listen_open(){
+            global $wpdb;
+
+            $table_store = TP_STORES_TABLE;
+            $table_revision = TP_REVISIONS_TABLE;
+            $table_address = DV_ADDRESS_TABLE;
+            $table_dv_revision = DV_REVS_TABLE;
+            $table_brgy = DV_BRGY_TABLE;
+            $table_city = DV_CITY_TABLE;
+            $table_province = DV_PROVINCE_TABLE;
+            $table_country = DV_COUNTRY_TABLE;
+            $table_category = TP_CATEGORIES_TABLE;
+            $table_contacts = DV_CONTACTS_TABLE;
+            $table_dv_revisions = DV_REVS_TABLE;
+            
+            // Step 1: Check if prerequisites plugin are missing
+            $plugin = TP_Globals::verify_prerequisites();
+            if ($plugin !== true) {
+                return array(
+                        "status" => "unknown",
+                        "message" => "Please contact your administrator. ".$plugin." plugin missing!",
+                );
+            }
+
+            // Step 2: Validate user
+            if (DV_Verification::is_verified() == false) {
+                return array(
+                        "status" => "unknown",
+                        "message" => "Please contact your administrator. Verification issues!",
+                );
+            }
+
+            // Step 2: Sanitize Request
+            if (!isset($_POST['lat']) || !isset($_POST['long']) ) {
+                return array(
+                    "status" => "unknown",
+                    "message" => "Please contact your administrator. Request unknown missing paramiters!",
+                );
+            }
+            
+            // Step 2: Sanitize Request
+            if (empty($_POST['lat']) || empty($_POST['long']) ) {
+                return array(
+                    "status" => "failed",
+                    "message" => "Required fileds cannot be empty.",
+                );
+            }
+
+            $lat2 = $_POST['lat'];
+            $long2 = $_POST['long'];
+
+            // Step 4: Start query
+            $results = $wpdb->get_results(" SELECT
+                tp_str.ID,
+                ( SELECT tp_rev.child_val FROM $table_revision tp_rev WHERE ID = tp_str.short_info ) AS `short_info`,
+                ( SELECT tp_rev.child_val FROM $table_revision tp_rev WHERE ID = tp_str.long_info ) AS `long_info`,
+                ( SELECT tp_rev.child_val FROM $table_revision tp_rev WHERE ID = tp_str.logo ) AS `logo`,
+                ( SELECT tp_rev.child_val FROM $table_revision tp_rev WHERE ID = tp_str.banner ) AS `banner`,
+                ( SELECT dv_rev.child_val FROM $table_dv_revision dv_rev WHERE ID = dv_add.street ) AS `street`,
+                ( SELECT brgy_name FROM $table_brgy WHERE ID = ( SELECT child_val FROM $table_dv_revision WHERE ID = dv_add.brgy ) ) AS brgy,
+                ( SELECT city_name FROM $table_city WHERE city_code = ( SELECT child_val FROM $table_dv_revision WHERE ID = dv_add.city ) ) AS city,
+                ( SELECT prov_name FROM $table_province WHERE prov_code = ( SELECT child_val FROM $table_dv_revision WHERE ID = dv_add.province ) ) AS province,
+                ( SELECT country_name FROM $table_country WHERE ID = ( SELECT child_val FROM $table_dv_revision WHERE ID = dv_add.country ) ) AS country ,
+                ROUND((SELECT `distance`(( SELECT child_val FROM $table_dv_revision WHERE ID = dv_add.latitude ), ( SELECT child_val FROM $table_dv_revision WHERE ID = dv_add.longitude ) ,'$lat2', '$long2' ) ), 3)AS `distance`
+            FROM
+                $table_store tp_str
+                INNER JOIN $table_revision tp_rev ON tp_rev.ID = tp_str.`status` 
+                INNER JOIN $table_address dv_add ON tp_str.address = dv_add.ID	
+                WHERE tp_rev.child_val = 1 LIMIT 20
+            ");
+
+            // Step 5: Return results
+            if (!$results) {
+                return array(
+                    "status" => "success",
+                    "message" => "No results found."
+                );
+            }else{
+                return array(
+                    "status" => "success",
+                    "data" => $results
+                );
+            }
+
+        }
+    }
