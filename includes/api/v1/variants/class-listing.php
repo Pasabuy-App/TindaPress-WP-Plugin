7db2ca76b0b5981e9aa4052cc27f76d99dfd7e5c
Miguel Radaza <48942632+MiguelRadaza@users.noreply.github.com>
2020-08-22 13:45:41 +0800

    Fixed active, inactive of variants

--- includes/api/v1/variants/class-listing.php
+++ includes/api/v1/variants/class-listing.php
@@ -102,7 +102,7 @@
                 rev.child_val as 'name',
                 (SELECT child_val FROM tp_revisions rev WHERE parent_id = var.ID AND child_key = 'price' AND revs_type ='variants'  AND date_created = ( SELECT MAX(date_created) FROM tp_revisions WHERE ID = rev.ID  ) ) as `price`,
                 (SELECT child_val FROM tp_revisions rev WHERE parent_id = var.ID AND child_key = 'info' AND revs_type ='variants'  AND date_created = ( SELECT MAX(date_created) FROM tp_revisions WHERE ID = rev.ID  ) ) as `info`,
-                IF((SELECT child_val FROM tp_revisions rev WHERE parent_id = var.ID AND child_key = 'status' AND revs_type ='variants'  AND date_created = ( SELECT MAX(date_created) FROM tp_revisions WHERE ID = rev.ID  ) ) = 1, 'Active', IF((SELECT child_val FROM tp_revisions rev WHERE parent_id = var.ID AND child_key = 'status' AND revs_type ='variants'  AND date_created = ( SELECT MAX(date_created) FROM tp_revisions WHERE ID = rev.ID  ) ) is NULL , '0', 'Inactive' )) as `status`
+                IF((SELECT child_val FROM tp_revisions rev WHERE parent_id = var.ID AND child_key = 'status' AND revs_type ='variants'  AND date_created = ( SELECT MAX(date_created) FROM tp_revisions WHERE ID = rev.ID  ) ) = 1, 'Active', IF((SELECT child_val FROM tp_revisions rev WHERE parent_id = var.ID AND child_key = 'status' AND revs_type ='variants'  AND date_created = ( SELECT MAX(date_created) FROM tp_revisions WHERE ID = rev.ID  ) ) is NULL , 'Inactive', 'Inactive' )) as `status`
             FROM
                 tp_revisions rev
                 INNER JOIN tp_variants var ON var.parent_id = '$parent' 
