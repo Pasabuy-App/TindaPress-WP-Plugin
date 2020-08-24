8230b8ddb54756bb8adc8d464c0ec33534c616d0
Miguel Radaza <48942632+MiguelRadaza@users.noreply.github.com>
2020-08-24 23:15:58 +0800

    Initial QA for listing variants

--- includes/api/v1/variants/class-listing.php
+++ includes/api/v1/variants/class-listing.php
@@ -19,6 +19,7 @@
 
         public static function list_variants(){
             
+            // 2nd Initial QA 2020-08-24 11:12 PM - Miguel
             global $wpdb;
             $table_variants = TP_VARIANTS_TABLE;
             $table_revs = TP_REVISIONS_TABLE;
@@ -29,16 +30,16 @@
             $plugin = TP_Globals::verify_prerequisites();
             if ($plugin !== true) {
                 return array(
-                        "status" => "unknown",
-                        "message" => "Please contact your administrator. ".$plugin." plugin missing!",
+                    "status" => "unknown",
+                    "message" => "Please contact your administrator. ".$plugin." plugin missing!",
                 );
             }
 
             // Step2 : Check if wpid and snky is valid
             if (DV_Verification::is_verified() == false) {
                 return array(
-                        "status" => "unknown",
-                        "message" => "Please contact your administrator. Verification Issues!",
+                    "status" => "unknown",
+                    "message" => "Please contact your administrator. Verification Issues!",
                 );
             }
 
@@ -48,31 +49,29 @@
             isset($_POST['vrid']) ? $vrid = $_POST['vrid'] : $vrid = NULL;
             isset($_POST['type']) ? $typ = $_POST['type'] : $typ = NULL;
 
-
             $status = $sts  == '0' || $sts == NULL ? NULL : ($sts == '2' && $sts !== '0'? '0':'1');
             $product_id = $pdid  == '0'  || $pdid == NULL ? NULL: $product_id = $pdid;
             $parent_id = $pid  == '0' || $pid == NULL ? NULL: $parent_id = $pid;
             $variants_id = $vrid  == '0' || $vrid == NULL ? NULL: $variants_id = $vrid;
             $type = $typ  == '0' || $typ == NULL ? NULL: $type = $typ;
             
-            
             $sql = "SELECT
-            var.ID,
-            var.pdid,
-            ( SELECT child_val FROM tp_revisions rev WHERE  rev.parent_id = var.ID AND rev.child_key = 'name' AND rev.revs_type = 'variants' ) as `name`,
-            ( SELECT child_val FROM tp_revisions rev WHERE  rev.parent_id = var.ID AND rev.child_key = 'info' AND rev.revs_type = 'variants' ) as `info`,
-            IF( ( SELECT child_val FROM tp_revisions rev WHERE  rev.parent_id = var.ID AND rev.child_key = 'baseprice' AND rev.revs_type = 'variants' ) = 1, 'Yes', 'No') as `base`,
+                var.ID,
+                var.pdid,
+                ( SELECT child_val FROM $table_revs rev WHERE  rev.parent_id = var.ID AND rev.child_key = 'name' AND rev.revs_type = 'variants' ) as `name`,
+                ( SELECT child_val FROM $table_revs rev WHERE  rev.parent_id = var.ID AND rev.child_key = 'info' AND rev.revs_type = 'variants' ) as `info`,
+                IF( ( SELECT child_val FROM $table_revs rev WHERE  rev.parent_id = var.ID AND rev.child_key = 'baseprice' AND rev.revs_type = 'variants' ) = 1, 'Yes', 'No') as `base`,
 
-                IF( rev.child_val = 1 , 'Active','Inactive') AS `status`,
-                null as options
-            FROM
-                tp_variants var
-                INNER JOIN tp_revisions rev ON rev.parent_id = var.ID 
-            WHERE
-                var.parent_id = 0
-                AND rev.revs_type = 'variants' 
-                AND child_key = 'status' 
-                AND rev.ID = ( SELECT MAX( ID ) FROM tp_revisions WHERE parent_id = rev.parent_id AND child_key = rev.child_key )
+                    IF( rev.child_val = 1 , 'Active','Inactive') AS `status`,
+                    null as options
+                FROM
+                    $table_variants var
+                    INNER JOIN $table_revs rev ON rev.parent_id = var.ID 
+                WHERE
+                    var.parent_id = 0
+                    AND rev.revs_type = 'variants' 
+                    AND child_key = 'status' 
+                    AND rev.ID = ( SELECT MAX( ID ) FROM $table_revs WHERE parent_id = rev.parent_id AND child_key = rev.child_key )
             ";
 
             if (isset($_POST['pdid'])) {
@@ -99,15 +98,15 @@
                 
                 $parent = $value->ID;
                 $option = $wpdb->get_results("SELECT
-                    rev.ID,
-                    rev.child_val as 'name',
-                    (SELECT child_val FROM tp_revisions rev WHERE parent_id = var.ID AND child_key = 'price' AND revs_type ='variants'  AND date_created = ( SELECT MAX(date_created) FROM tp_revisions WHERE ID = rev.ID  ) ) as `price`,
-                    (SELECT child_val FROM tp_revisions rev WHERE parent_id = var.ID AND child_key = 'info' AND revs_type ='variants'  AND date_created = ( SELECT MAX(date_created) FROM tp_revisions WHERE ID = rev.ID  ) ) as `info`,
-                    IF( ( SELECT child_val FROM tp_revisions rev WHERE parent_id = var.ID AND child_key = 'status' AND revs_type ='variants' AND ID = ( SELECT MAX(ID) FROM tp_revisions WHERE child_key ='status' AND revs_type ='variants'  ) ) = 1, 'Active', 'Inactive' ) as `status`
-                FROM
-                    tp_revisions rev
-                    INNER JOIN tp_variants var ON var.parent_id = '$parent' 
-                WHERE   rev.revs_type = 'variants' AND child_key = 'name' AND rev.parent_id = var.ID
+                        rev.ID,
+                        rev.child_val as 'name',
+                        (SELECT child_val FROM $table_revs rev WHERE parent_id = var.ID AND child_key = 'price' AND revs_type ='variants'  AND date_created = ( SELECT MAX(date_created) FROM $table_revs WHERE ID = rev.ID  ) ) as `price`,
+                        (SELECT child_val FROM $table_revs rev WHERE parent_id = var.ID AND child_key = 'info' AND revs_type ='variants'  AND date_created = ( SELECT MAX(date_created) FROM $table_revs WHERE ID = rev.ID  ) ) as `info`,
+                        IF( ( SELECT child_val FROM $table_revs rev WHERE parent_id = var.ID AND child_key = 'status' AND revs_type ='variants' AND ID = ( SELECT MAX(ID) FROM $table_revs WHERE child_key ='status' AND revs_type ='variants'  ) ) = 1, 'Active', 'Inactive' ) as `status`
+                    FROM
+                        $table_revs rev
+                        INNER JOIN $table_variants var ON var.parent_id = '$parent' 
+                    WHERE   rev.revs_type = 'variants' AND child_key = 'name' AND rev.parent_id = var.ID
                 ");
 
                 if (isset( $_POST['vrid'] ) && isset($_POST['pdid'])) {
@@ -122,20 +121,11 @@
                         $value->options = $option;
                     }
                 }
-
-              
-
-                           
-
             }
-                  
-        
-
 
             return array(
                 "status" => "success",
                 "data" => $result
             );
-
         }   
     }
