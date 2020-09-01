ed95901c2744e9e873684dec256c4b83d67449b8
BytesCrafter <bytescrafter@gmail.com>
2020-09-01 09:54:40 +0800

    Added TP_UIHOST value for live site.

--- includes/core/config.php
+++ includes/core/config.php
@@ -16,8 +16,8 @@
 	//Defining Global Variables
 	define('TP_CUSTOM', 'eCommerce'); 
 	define('TP_PREFIX', 'tp_'); 
-	define('TP_UIHOST', 'https://wphost-b.dev');
-	define('TP_FULLMODE', false);
+	define('TP_UIHOST', 'https://pasabuy.app');
+	define('TP_FULLMODE', true);
 	
 	//Categories config
 	define('TP_CATEGORIES_TABLE', TP_PREFIX.'categories');
