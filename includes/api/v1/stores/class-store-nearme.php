46965f02ace5106dc18c8394602d748f1466ee07
Miguel Radaza <miguel1.radaza@gmail.com>
2020-10-08 19:10:14 +0800

    Revised store nearme calculating distance by 6 kilometers

--- includes/api/v1/stores/class-store-nearme.php
+++ includes/api/v1/stores/class-store-nearme.php
@@ -1,20 +1,20 @@
 <?php
 	// Exit if accessed directly
-	if ( ! defined( 'ABSPATH' ) ) 
+	if ( ! defined( 'ABSPATH' ) )
 	{
 		exit;
 	}
 
-	/** 
+	/**
         * @package tindapress-wp-plugin
         * @version 0.1.0
 	*/
 
     class TP_Store_Nearme {
-        
+
         //REST API Call
         public static function listen(){
-            return rest_ensure_response( 
+            return rest_ensure_response(
                 TP_Store_Nearme:: listen_open()
             );
         }
@@ -35,7 +35,8 @@
             $table_category = TP_CATEGORIES_TABLE;
             $table_contacts = DV_CONTACTS_TABLE;
             $table_dv_revisions = DV_REVS_TABLE;
-            
+            $date = TP_Globals::date_stamp();
+
             // Step 1: Check if prerequisites plugin are missing
             $plugin = TP_Globals::verify_prerequisites();
             if ($plugin !== true) {
@@ -46,21 +47,21 @@
             }
 
             // Step 2: Validate user
-            if (DV_Verification::is_verified() == false) {
+           /*  if (DV_Verification::is_verified() == false) {
                 return array(
                     "status" => "unknown",
                     "message" => "Please contact your administrator. Verification issues!",
                 );
-            }
+            } */
 
             // Step 2: Sanitize Request
             if (!isset($_POST['lat']) || !isset($_POST['long']) ) {
                 return array(
                     "status" => "unknown",
-                    "message" => "Please contact your administrator. Request unknown missing paramiters!",
+                    "message" => "Please contact your administrator. Request unknown!",
                 );
             }
-            
+
             // Step 2: Sanitize Request
             if (empty($_POST['lat']) || empty($_POST['long']) ) {
                 return array(
@@ -69,28 +70,63 @@
                 );
             }
 
-            $lat2 = $_POST['lat'];
-            $long2 = $_POST['long'];
+            $lat = $_POST['lat'];
+            $long = $_POST['long'];
 
             // Step 4: Start query
-            $results = $wpdb->get_results(" SELECT
-                tp_str.ID,
-                ( SELECT tp_rev.child_val FROM $table_revision tp_rev WHERE ID = tp_str.short_info ) AS `short_info`,
-                ( SELECT tp_rev.child_val FROM $table_revision tp_rev WHERE ID = tp_str.long_info ) AS `long_info`,
-                ( SELECT tp_rev.child_val FROM $table_revision tp_rev WHERE ID = tp_str.logo ) AS `logo`,
-                ( SELECT tp_rev.child_val FROM $table_revision tp_rev WHERE ID = tp_str.banner ) AS `banner`,
-                ( SELECT dv_rev.child_val FROM $table_dv_revision dv_rev WHERE ID = dv_add.street ) AS `street`,
-                ( SELECT brgy_name FROM $table_brgy WHERE ID = ( SELECT child_val FROM $table_dv_revision WHERE ID = dv_add.brgy ) ) AS brgy,
-                ( SELECT city_name FROM $table_city WHERE city_code = ( SELECT child_val FROM $table_dv_revision WHERE ID = dv_add.city ) ) AS city,
-                ( SELECT prov_name FROM $table_province WHERE prov_code = ( SELECT child_val FROM $table_dv_revision WHERE ID = dv_add.province ) ) AS province,
-                ( SELECT country_name FROM $table_country WHERE ID = ( SELECT child_val FROM $table_dv_revision WHERE ID = dv_add.country ) ) AS country ,
-                ROUND((SELECT `hp_distance_k`(( SELECT child_val FROM $table_dv_revision WHERE ID = dv_add.latitude ), ( SELECT child_val FROM $table_dv_revision WHERE ID = dv_add.longitude ) ,'$lat2', '$long2' ) ), 3)AS `distance`
-            FROM
-                $table_store tp_str
-                INNER JOIN $table_revision tp_rev ON tp_rev.ID = tp_str.`status` 
-                INNER JOIN $table_address dv_add ON tp_str.address = dv_add.ID	
-                WHERE tp_rev.child_val = 1 LIMIT 20
+            $results = $wpdb->get_results("SELECT
+            tp_str.ID,
+            ( SELECT rev.child_val FROM tp_revisions rev WHERE rev.id = tp_str.title AND  rev.date_created = ( SELECT MAX(date_created) FROM tp_revisions tp_rev WHERE tp_rev.ID = rev.ID AND revs_type = 'stores' )  ) AS title,
+            ( SELECT tp_rev.child_val FROM tp_revisions tp_rev WHERE ID = tp_str.short_info ) AS `short_info`,
+            ( SELECT tp_rev.child_val FROM tp_revisions tp_rev WHERE ID = tp_str.long_info ) AS `long_info`,
+            ( SELECT tp_rev.child_val FROM tp_revisions tp_rev WHERE ID = tp_str.logo ) AS `logo`,
+            ( SELECT tp_rev.child_val FROM tp_revisions tp_rev WHERE ID = tp_str.banner ) AS `banner`,
+            ( SELECT dv_rev.child_val FROM dv_revisions dv_rev WHERE ID = dv_add.street ) AS `street`,
+            ( SELECT brgy_name FROM dv_geo_brgys WHERE ID = ( SELECT child_val FROM dv_revisions WHERE ID = dv_add.brgy ) ) AS brgy,
+            ( SELECT city_name FROM dv_geo_cities WHERE city_code = ( SELECT child_val FROM dv_revisions WHERE ID = dv_add.city ) ) AS city,
+            ( SELECT prov_name FROM dv_geo_provinces WHERE prov_code = ( SELECT child_val FROM dv_revisions WHERE ID = dv_add.province ) ) AS province,
+            ( SELECT country_name FROM dv_geo_countries WHERE ID = ( SELECT child_val FROM dv_revisions WHERE ID = dv_add.country ) ) AS country,
+            (SELECT date_close FROM mp_operations WHERE stid = tp_str.ID) as date_close,
+            (SELECT dv_revisions.child_val FROM dv_address INNER JOIN dv_revisions ON  dv_address.latitude = dv_revisions.ID WHERE dv_address.stid =  tp_str.ID) as store_lat,
+            (SELECT dv_revisions.child_val FROM dv_address INNER JOIN dv_revisions ON  dv_address.longitude = dv_revisions.ID WHERE dv_address.stid =  tp_str.ID) as store_long
+        FROM
+            tp_stores tp_str
+            INNER JOIN tp_revisions tp_rev ON tp_rev.ID = tp_str.`status`
+            INNER JOIN dv_address dv_add ON tp_str.address = dv_add.ID
+        WHERE
+            -- Validating status to active
+            tp_rev.child_val = 1
+            LIMIT 20;
             ");
+
+            // Remove store from array if store is close
+            foreach ($results as $key => $value) {
+                $store_id = $value->ID;
+                $get_date_close = $wpdb->get_row("SELECT date_close FROM mp_operations WHERE stid = '$store_id'");
+
+
+
+                if (!empty($get_date_close) ) {
+
+                    $origin = new DateTime($get_date_close->date_close);
+                    $target = new DateTime('2020-10-08 05:59:17');
+
+                    $interval = $origin->diff($target);
+                    $smp = $interval->format('%R%a days');
+
+                    if ($smp <= 0) {
+                       //unset($results[$key]);
+                       array_splice($results, $key, $key);
+                    }
+                }
+
+                $get_distance = TP_Globals::GetDistance($lat, $long, $value->store_lat, $value->store_long, 'Km' );
+
+                if ($get_distance > 6) {
+                    array_splice($results, $key, $key);
+                }
+            }
+
 
             // Step 5: Return results
             return array(
@@ -98,4 +134,11 @@
                 "data" => $results
             );
         }
+
+        public static function set($results,$key){
+
+        }
+
+
+
     }
