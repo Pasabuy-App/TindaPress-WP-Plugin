6c08ff15c84b22b60ed81f49e3db63e03c90b339
Miguel Radaza <miguel1.radaza@gmail.com>
2020-10-19 14:34:35 +0800

    Fixe bugs in deleting store

--- includes/api/v1/stores/class-delete.php
+++ includes/api/v1/stores/class-delete.php
@@ -1,11 +1,11 @@
 <?php
 	// Exit if accessed directly
-	if ( ! defined( 'ABSPATH' ) ) 
+	if ( ! defined( 'ABSPATH' ) )
 	{
 		exit;
 	}
 
-	/** 
+	/**
         * @package tindapress-wp-plugin
         * @version 0.1.0
 	*/
@@ -15,17 +15,17 @@
     class TP_Delete_Store {
 
         public static function listen(){
-            return rest_ensure_response( 
+            return rest_ensure_response(
                 TP_Delete_Store::list_open()
             );
         }
-        
+
         // 2nd Initial QA 2020-08-24 10:38 PM - Miguel
         //QA done 2020-08-12 10:18 pm
         public static function list_open(){
 
             global $wpdb;
-            
+
             $user = TP_Delete_Store::catch_post();
 
             // declaring table names to variable
@@ -42,7 +42,7 @@
                     "message" => "Please contact your administrator. ".$plugin." plugin missing!",
                 );
             }
-            
+
             // Step 2: Validate user
             if (DV_Verification::is_verified() == false) {
                 return array(
@@ -58,7 +58,7 @@
                     "message" => "Please contact your administrator. Missing paramiters.",
                 );
             }
-            
+
             // Step 4: Check if parameters passed are empty
             if (empty($_POST["stid"])) {
                 return array(
@@ -66,10 +66,10 @@
                     "message" => "Required fields cannot be empty.",
                 );
             }
-            
+
             // Step 5: Check if store exists in database.
             $store_data = $wpdb->get_row("SELECT child_val as stats FROM tp_revisions WHERE ID = (SELECT `status` FROM tp_stores WHERE ID = '{$user["store_id"]}')");
-               
+
             // Check if no rows found
             if (!$store_data) {
                 return array(
@@ -77,7 +77,7 @@
                     "message" => "This store does not exists.",
                 );
             }
-            
+
             //Fails if status is already 0, or deactivated
             if ($store_data->stats == 0) {
                 return array(
@@ -99,42 +99,28 @@
                     (select child_val from $table_revisions where id = tp_str.status) AS `status`
                 FROM
                     $table_store tp_str
-                INNER JOIN 
-                    $table_revisions tp_rev ON tp_rev.ID = tp_str.title 
-                WHERE 
+                INNER JOIN
+                    $table_revisions tp_rev ON tp_rev.ID = tp_str.title
+                WHERE
                     tp_str.ID = '{$user["store_id"]}'
                 ");
 
-                 //Inserting new data using the current value
-                $wpdb->query("INSERT INTO $table_revisions $table_revision_field VALUES ( 'stores', '{$user["store_id"]}', 'title', '$get_last_value->title', '{$user["created_by"]}', '$date_created'  ) ");
-                $title = $wpdb->insert_id;
-
-                $wpdb->query("INSERT INTO $table_revisions $table_revision_field VALUES ( 'stores', '{$user["store_id"]}', 'short_info', '$get_last_value->short_info', '{$user["created_by"]}', '$date_created'  ) ");
-                $short_info = $wpdb->insert_id;
-
-                $wpdb->query("INSERT INTO $table_revisions $table_revision_field VALUES ( 'stores', '{$user["store_id"]}', 'long_info', '$get_last_value->long_info', '{$user["created_by"]}', '$date_created'  ) ");
-                $long_info = $wpdb->insert_id;
-
-                $wpdb->query("INSERT INTO $table_revisions $table_revision_field VALUES ( 'stores', '{$user["store_id"]}', 'logo', '$get_last_value->logo', '{$user["created_by"]}', '$date_created'  ) ");
-                $logo = $wpdb->insert_id;
-
-                $wpdb->query("INSERT INTO $table_revisions $table_revision_field VALUES ( 'stores', '{$user["store_id"]}', 'banner', '$get_last_value->banner', '{$user["created_by"]}', '$date_created'  ) ");
-                $banner = $wpdb->insert_id;
+                //Inserting new data using the current value
 
                 $wpdb->query("INSERT INTO $table_revisions $table_revision_field VALUES ( 'stores', '{$user["store_id"]}', 'status', '0', '{$user["created_by"]}', '$date_created'  ) ");
                 $status = $wpdb->insert_id;
 
-                $update_store = $wpdb->query("UPDATE tp_stores SET `title` = '$title', `short_info` = '$short_info', `long_info` = '$long_info', `logo` = '$logo', `banner` = '$banner', `status` = '$status' WHERE ID = '{$user["store_id"]}' ");
+                $update_store = $wpdb->query("UPDATE tp_stores SET `status` = '$status' WHERE ID = '{$user["store_id"]}' ");
 
             // Step 7: Check if any queries above failed
-            if ($title < 1 || $short_info < 1 || $long_info < 1 || $logo < 1 || $banner < 1 || $status < 1 || $update_store < 1 ) {
+            if ($status < 1 || $update_store < 1 ) {
                 //Do a rollback if any of the above queries failed
                 $wpdb->query("ROLLBACK");
                 return array(
                     "status" => "failed",
                     "message" => "An error occured while submmiting data to database.",
                 );
-                
+
             } else{
                 //Commit if no errors found
                 $wpdb->query("COMMIT");
@@ -143,16 +129,16 @@
                     "message" => "Data has been deleted successfully.",
                 );
             }
-        }  
-        
-        // Catch Post 
+        }
+
+        // Catch Post
         public static function catch_post()
         {
             $cur_user = array();
-               
+
             $cur_user['created_by'] = $_POST["wpid"];
             $cur_user['store_id']      = $_POST["stid"];
-  
+
             return  $cur_user;
         }
     }
